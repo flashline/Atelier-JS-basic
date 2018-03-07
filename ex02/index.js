51da883174119b05f23443a62a95fdd50c92c636
jean-michel delettre <info@pixaline.net>
2018-03-07 14:26:08 +0100

    add utils

--- ex02/index.js
+++ ex02/index.js
@@ -2,9 +2,16 @@
 
 // fonctions
 
- // Les fonctions utilitaires
- 
- 
+/// Les fonctions écouteur d'événement
+
+/// Les fonctions utilitaires
+function $(id) 		{ return document.getElementById(id);} 
+function affich(tx) { $("zoneAffich").innerHTML+=tx+"<br/>";}
+function efface() 	{ $("zoneAffich").innerHTML=""; $("zoneAffich").style.color="black"; }
+function addons () 	{
+	if (!Element.prototype.on)  Element.prototype.on=Element.prototype.addEventListener;
+	if (!Element.prototype.off) Element.prototype.off=Element.prototype.removeEventListener;	
+} 
  /* 
 	/[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z][A-Z][A-Z]?/i; 
  */
