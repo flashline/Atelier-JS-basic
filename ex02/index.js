ab8d503e2e9e9f7b51b63d302c9e3eadaefeaf24
jean-michel delettre <info@pixaline.net>
2019-03-01 14:39:35 +0100

    Update index.js

--- ex02/index.js
+++ ex02/index.js
@@ -10,14 +10,63 @@ $("boxEnvoi").style.display="none";
 
 /// Les fonctions écouteur d'événement
 function valideListener (e) {
-	var msg=""; 
-	if ($("prenom").value==="") msg+="prenom manquant <br>" ;
+	var msg="";  
+	if ($("prenom").value=="") msg+="prenom manquant <br>" ;
+	if ($("nom").value=="") msg+="nom manquant <br>" ;
+	var inputAge=parseInt($("age").value);
+	if (Number.isNaN(inputAge) || inputAge<12 || inputAge>120) msg+="age invalide <br>" ;
+	//
+	var re=/[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z][A-Z][A-Z]?/i;
+	if (!$("email").value.match(re)) msg+="email invalide <br>";
+	efface();
+	if (msg!="") {
+		$("zoneAffich").style.color="#ff0000"; // "red";
+		affich(msg);
+	}
+	else {
+		var eleve={};
+		eleve.prenom=$("prenom").value;
+		eleve.nom=$("nom").value;
+		eleve.age=inputAge;
+		eleve.mail=$("email").value;
+		eleve.choix=$("choix").value;
+		eleve.versAffich=versAffich;
+		// 
+		eleves.push(eleve);
+		affich("<b>inscription bien effectuée</b>");
+		eleve.versAffich();	
+		razForm();
+	}
+	
+}
+function versAfffich () {
+	affich(this.prenom+" "+this.nom+" agé(e) de "+this.age+" ans est inscris en "+ this.choix);
+}
+function razForm () {
+	$("prenom").value="";
+	$("nom").value="";
+	$("age").value="";
+	$("email").value="";
+	//$("choix").value="JavaScript";
+	$("info").value="";
 }
 function razFormListener (e) {
-	affich("raz");
+	razForm();
 }
 function termineListener (e) {
-	affich("termine");
+	efface();
+	affich("<b>Récapitulatif de saisie</b>");
+	for (i=0;i<eleves.length;i++) {
+		eleves[i].versAffich();
+		affich("----------------------------");
+	}
+	//
+	$("boxEnvoi").style.display="block";
+	$("envoi").addEventListener("click",envoiListener);
+}
+function envoiListener (e) {
+	$("recapMail").value=$("zoneAffich").innerHTML ;
+	$("forMail").submit();
 }
 /// Les fonctions utilitaires
 function $(id) 		{ return document.getElementById(id);} 
@@ -27,7 +76,7 @@ function addons () 	{
 	if (!Element.prototype.on)  Element.prototype.on=Element.prototype.addEventListener;
 	if (!Element.prototype.off) Element.prototype.off=Element.prototype.removeEventListener;	
 } 
- /* 
+/* 
 	/[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z][A-Z][A-Z]?/i; 
  */
  	
